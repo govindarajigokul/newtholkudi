f40c6b62bfacd253291b8fa449a0b4e2fdbc2ccd
govindarajigokul <govindarajigokul@gmail.com>
2025-08-22 00:08:00 +0530

    header dropdown added

--- src/themes/dspace/app/header/header.component.ts
+++ src/themes/dspace/app/header/header.component.ts
@@ -6,12 +6,17 @@ import {
   Component,
   OnInit,
   HostListener,
+  inject,
 } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { ThemedLangSwitchComponent } from 'src/app/shared/lang-switch/themed-lang-switch.component';
+
+import { BrowseDefinitionDataService } from '../../../../app/core/browse/browse-definition-data.service';
+import { getFirstCompletedRemoteData } from '../../../../app/core/shared/operators';
 
 import { ContextHelpToggleComponent } from '../../../../app/header/context-help-toggle/context-help-toggle.component';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
@@ -35,6 +40,10 @@ export class HeaderComponent extends BaseComponent implements OnInit {
 
   // Mobile menu state
   isMobileMenuOpen = false;
+
+  // Injected services
+  private router = inject(Router);
+  private browseDefinitionService = inject(BrowseDefinitionDataService);
 
   ngOnInit() {
     super.ngOnInit();
@@ -72,5 +81,50 @@ export class HeaderComponent extends BaseComponent implements OnInit {
         !hamburgerButton.contains(target)) {
       this.isMobileMenuOpen = false;
     }
+  }
+
+  /**
+   * Navigate to browse page with fallback to search
+   * @param browseType The browse type (author, title, subject, etc.)
+   */
+  navigateToBrowse(browseType: string): void {
+    // First try to check if browse definition exists
+    this.browseDefinitionService.findById(browseType).pipe(
+      getFirstCompletedRemoteData(),
+      take(1)
+    ).subscribe(browseDefinitionRD => {
+      if (browseDefinitionRD.hasSucceeded && browseDefinitionRD.payload) {
+        // Browse definition exists, navigate to browse page
+        this.router.navigate(['/browse', browseType]);
+      } else {
+        // Browse definition doesn't exist, fallback to search with filter
+        this.navigateToSearchFallback(browseType);
+      }
+    });
+  }
+
+  /**
+   * Fallback navigation to search page with appropriate filter
+   * @param browseType The browse type to convert to search filter
+   */
+  private navigateToSearchFallback(browseType: string): void {
+    const filterMap: { [key: string]: string } = {
+      'author': 'author',
+      'title': 'title',
+      'subject': 'subject',
+      'language': 'language',
+      'media': 'type',
+      'linguistictype': 'type'
+    };
+
+    const filterField = filterMap[browseType] || browseType;
+
+    // Navigate to search page with filter
+    this.router.navigate(['/search'], {
+      queryParams: {
+        [`f.${filterField}`]: '',
+        'view': 'list'
+      }
+    });
   }
 }
